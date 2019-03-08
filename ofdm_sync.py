9f0b93e1359ed75a2ff8c09f3f0cfdd3693eec96
arianaolson419 <arianaolson419@gmail.com>
2019-03-08 11:51:55 -0500

    Import libraries

---
+++ ofdm_sync.py
@@ -0,0 +1,8 @@
+"""This script contains code to implement a simulated OFDM system, as described
+in Lab 3 part a. The signals in this script are timing synchronized.
+
+Author: Ariana Olson
+"""
+from __future__ import print_function, division
+
+from utils import ofdm_util
