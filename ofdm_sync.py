ddd76f048c47fbd383e1af54c250eba51ea10442
arianaolson419 <arianaolson419@gmail.com>
2019-03-11 13:15:18 -0400

    Import channel simulation code

--- ofdm_sync.py
+++ ofdm_sync.py
@@ -6,3 +6,12 @@ Author: Ariana Olson
 from __future__ import print_function, division
 
 from utils import ofdm_util
+from utils import nonflat_channel.py
+
+# Create a signal to transmit.
+# Create a short known signal to use for channel estimation.
+# Transmit signal through the channel.
+# Estimate the channel with the known signal.
+# Convert from time to frequency domain.
+# Equalize the signal
+# decode the signal in the frequency domain.
